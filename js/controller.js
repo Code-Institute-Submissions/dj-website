60302dce49620d7d5b29f1718ac66361d397cc91
Darren <dazfrancisllpg@hotmail.com>
2016-10-31 09:25:09 +0000

    add model for gigs, mix tl's and product

--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,34 @@
 angular.module('RouteController', [])
 	.controller('HomeController', function($scope, $location) {
 		$scope.title = "DJ Website";
+
+		$scope.gigList = allGigs;
+		$scope.gigLimit = 2;
+		$scope.djShopProd = djTShirtBlack;
+		console.log($scope.djShopProd.img1);
 	})
 
 	.controller('MixesController', function($scope, $location) {
-		$scope.title = "Mixes";
+		$scope.tracklist = true;
+		$scope.showTl = function() {
+			$scope.tracklist = false;
+		};
+		$scope.hideTl = function() {
+			$scope.tracklist = true;
+		};
+
+		$scope.tracks = dnbTracks;
+		
+
+		
+
+
 	})
 
 	.controller('GigsController', function($scope, $location) {
 		$scope.title = "Gigs";
+
+		$scope.gigList = allGigs;
 	})
 
 	.controller('ShopController', function($scope, $location) {
@@ -18,4 +38,5 @@ angular.module('RouteController', [])
 	.controller('ContactController', function($scope, $location) {
 		$scope.title = "Contact";
 	})
+
 
